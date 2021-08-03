4be44a6b0c54067020c023e6245151591947244a
brandon@brandongulla.com <brandon@brandongulla.com>
2021-08-02 23:48:36 -0400

    adding env checker

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-/usr/local/bin/python -m dump1090exporter --resource-path=${resource-path}
+echo "RESOURCE_PATH=${RESOURCE_PATH}"
+
+if [  -z "${RESOURCE_PATH}" ];then
+  echo "[FATAL] RESOURCE_PATH env not set"
+  sleep 10
+  exit 1
+fi
+/usr/local/bin/python -m dump1090exporter --resource-path=${RESOURCE_PATH}
